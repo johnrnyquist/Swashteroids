8ead75d24988937d42fbf22664556e9d784974ad
johnrnyquist <john@nyquist.net>
2024-06-25 17:01:13 -0600

    I've temporarily commented this out

--- Swashteroids/systems/MakeDecisionSystem.swift
+++ Swashteroids/systems/MakeDecisionSystem.swift
@@ -1,33 +1,33 @@
+////
+//// https://github.com/johnrnyquist/Swashteroids
+////
+//// Download Swashteroids from the App Store:
+//// https://apps.apple.com/us/app/swashteroids/id6472061502
+////
+//// Made with Swash, give it a try!
+//// https://github.com/johnrnyquist/Swash
+////
 //
-// https://github.com/johnrnyquist/Swashteroids
+//import Foundation
+//import Swash
 //
-// Download Swashteroids from the App Store:
-// https://apps.apple.com/us/app/swashteroids/id6472061502
+//final class MakeDecisionSystem: ListIteratingSystem {
+//    init() {
+//        super.init(nodeClass: MakeDecisionNode.self)
+//        nodeUpdateFunction = updateNode
+//    }
 //
-// Made with Swash, give it a try!
-// https://github.com/johnrnyquist/Swash
+//    func updateNode(node: Node, time: TimeInterval) {
+//        guard let _ = node[MakeDecisionComponent.self],
+//              let reactionTimeComponent = node[ReactionTimeComponent.self],
+//              let entity = node.entity
+//        else { return }
+//        entity.remove(componentClass: MakeDecisionComponent.self)
+//        // In theory, I'll have more options here in the future
+//        entity.add(component: PickTargetComponent())
+//    }
+//}
 //
-
-import Foundation
-import Swash
-
-final class MakeDecisionSystem: ListIteratingSystem {
-    init() {
-        super.init(nodeClass: MakeDecisionNode.self)
-        nodeUpdateFunction = updateNode
-    }
-
-    func updateNode(node: Node, time: TimeInterval) {
-        guard let component = node[MakeDecisionComponent.self],
-              let reactionTimeComponent = node[ReactionTimeComponent.self],
-              let entity = node.entity
-        else { return }
-        entity.remove(componentClass: type(of: component))
-        reactionTimeComponent.reacted()
-        entity.add(component: PickTargetComponent())
-    }
-}
-
-
-
-
+//
+//
+//
