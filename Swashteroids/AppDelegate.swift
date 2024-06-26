fd4cc77b134e8523bdab2eb9309500133b5f0246
johnrnyquist <john@nyquist.net>
2024-06-26 17:53:33 -0600

    Refactoring

--- Swashteroids/AppDelegate.swift
+++ Swashteroids/AppDelegate.swift
@@ -34,44 +34,35 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        save()
-        gameViewController.showPauseAlert()
+        // Sent when the application is about to move from active to inactive state. 
+        // This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
+        // or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. 
+        // Games should use this method to pause the game.
+        gameViewController.appWillResignActive()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough 
+        // application state information to restore your application to its current state in case it is terminated later.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        // Called as part of the transition from the background to the active state; here you can undo many of 
+        // the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        if !gameViewController.isAlertPresented {
-            gameViewController.game?.start()
-        }
-    }
-
-    func save() {
-        if let entities = gameViewController.game?.engine.entities {
-            for entity in entities {
-                print(entity.name)
-                for component in entity.components {
-                    print("\t", type(of: component).name)
-                }
-                print("---")
-            }
-        }
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. 
+        // If the application was previously in the background, optionally refresh the user interface.
+        gameViewController.appDidBecomeActive()
     }
 }
 
 var appVersion: String {
-    return Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+    Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
 }
 var appBuild: String {
-    return Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
+    Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) as! String
 }
 
--- Swashteroids/GameViewController.swift
+++ Swashteroids/GameViewController.swift
@@ -22,7 +22,7 @@ protocol AlertPresenting: AnyObject {
 final class GameViewController: UIViewController, AlertPresenting {
     var skView: SKView?
     var gameScene: GameScene?
-    var game: Swashteroids?
+    var swashteroids: Swashteroids?
     var isAlertPresented = false //HACK: this is a hack to prevent the game from starting when the app returns from background.
 
     override func loadView() {
@@ -35,20 +35,20 @@ final class GameViewController: UIViewController, AlertPresenting {
     }
 
     func startNewGame() {
-        setupSKView()
-        createGameScene()
-        initializeGame()
-        presentGameScene()
+        skView_setup()
+        gameScene_create()
+        swashteroids_create()
+        gameScene_present()
     }
 
-    private func setupSKView() {
+    private func skView_setup() {
         skView?.showsPhysics = true
         skView?.ignoresSiblingOrder = true // true is more optimized rendering, but must set zPosition
         skView?.isUserInteractionEnabled = true
         skView?.isMultipleTouchEnabled = true
     }
 
-    private func createGameScene() {
+    private func gameScene_create() {
         let screenSize = UIScreen.main.bounds
         gameScene = GameScene(size: screenSize.size)
         gameScene?.name = "gameScene"
@@ -56,18 +56,18 @@ final class GameViewController: UIViewController, AlertPresenting {
         gameScene?.scaleMode = .aspectFit
     }
 
-    private func initializeGame() {
+    private func swashteroids_create() {
         guard let gameScene else { fatalError("gameScene is nil") }
         let seed = Int(Date().timeIntervalSince1970)
-        game = Swashteroids(scene: gameScene, alertPresenter: self, seed: seed)
-        gameScene.delegate = game
-        gameScene.touchDelegate = game
-        gameScene.physicsWorld.contactDelegate = game
+        swashteroids = Swashteroids(scene: gameScene, alertPresenter: self, seed: seed)
+        gameScene.delegate = swashteroids
+        gameScene.touchDelegate = swashteroids
+        gameScene.physicsWorld.contactDelegate = swashteroids
         gameScene.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-        game?.start()
+        swashteroids?.start()
     }
 
-    private func presentGameScene() {
+    private func gameScene_present() {
         skView?.presentScene(gameScene)
     }
 
@@ -81,19 +81,19 @@ final class GameViewController: UIViewController, AlertPresenting {
     func resume() {
         dismiss(animated: true, completion: { [unowned self] in
             isAlertPresented = false
-            game?.start()
+            swashteroids?.start()
         })
     }
 
     @IBAction func showPauseAlert() {
-        guard let game else { fatalError("game is nil") }
-        game.stop()
-        if game.engine.appStateComponent.appState == .playing ||
-           game.engine.appStateComponent.appState == .gameOver {
+        guard let swashteroids else { fatalError("game is nil") }
+        swashteroids.stop()
+        if swashteroids.engine.appStateComponent.appState == .playing ||
+           swashteroids.engine.appStateComponent.appState == .gameOver {
             let alertView = PauseAlert(
-                appState: game.engine.appStateComponent, //HACK
-                home: self.home,
-                resume: self.resume)
+                appState: swashteroids.engine.appStateComponent, //HACK
+                home: home,
+                resume: resume)
             let hostingController = UIHostingController(rootView: alertView)
             hostingController.modalPresentationStyle = .overCurrentContext
             hostingController.view.backgroundColor = UIColor(white: 1, alpha: 0.0)
@@ -102,15 +102,26 @@ final class GameViewController: UIViewController, AlertPresenting {
         }
     }
 
+    func appDidBecomeActive() {
+        if isAlertPresented == false {
+            swashteroids?.start()
+        }
+    }
+
+    func appWillResignActive() {
+        printAllComponents()
+        showPauseAlert()
+    }
+
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+            .allButUpsideDown
         } else {
-            return .all
+            .all
         }
     }
     override var prefersStatusBarHidden: Bool {
-        return true
+        true
     }
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -125,6 +136,18 @@ final class GameViewController: UIViewController, AlertPresenting {
             print("portraitUpsideDown")
         default:
             break
+        }
+    }
+
+    func printAllComponents() {
+        if let entities = swashteroids?.engine.entities {
+            for entity in entities {
+                print(entity.name)
+                for component in entity.components {
+                    print("\t", type(of: component).name)
+                }
+                print("---")
+            }
         }
     }
 }
